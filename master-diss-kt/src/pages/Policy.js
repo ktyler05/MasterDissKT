7e725ac48fca304ed6219f6abd96f86051f4fa56
ktyler05 <tylerk@cardiff.ac.uk>
2025-09-19 15:26:31 +0100

    Update Policy.js
    should actually work now

--- master-diss-kt/src/pages/Policy.js
+++ master-diss-kt/src/pages/Policy.js
@@ -3,11 +3,13 @@ import React from "react";
 function Policy() {
   return (
     <div className="page">
-      <h2>For PolicyMakers</h2>
-      <img src={parentImage} alt="Cyber Careers Hub" className="parent-image" />
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non eros vel lectus volutpat mattis ut nec metus. Proin sagittis, nisi nec rhoncus viverra.</p>
+      <h2>For Policymakers</h2>
+      <p>
+        Lorem ipsum dolor consecteur adipiscing elit. Nunc non eros vel lectus
+        volutpat mattis ut nec metus. Proin sagittis, nisi nec rhoncus viverra.
+      </p>
     </div>
   );
 }
 
-export default Parents;
+export default Policy;
