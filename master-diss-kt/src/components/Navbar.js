362dde2f1b2c23544e4c8da24d486cee41530e1e
ktyler05 <tylerk@cardiff.ac.uk>
2025-09-22 23:56:28 +0100

    more css fixes

--- master-diss-kt/src/components/Navbar.js
+++ master-diss-kt/src/components/Navbar.js
@@ -1,26 +1,40 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useRef, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "../styles.css";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  const location = useLocation();
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
-  // close dropdown when a link is clicked
+  const toggleDropdown = () => setIsOpen((v) => !v);
   const closeDropdown = () => setIsOpen(false);
+
+ 
+  useEffect(() => {
+    setIsOpen(false);
+  }, [location.pathname]);
+
+
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
 
   return (
     <nav className="navbar">
-      <h1 className="site-title">Cyber Career Paths</h1>
+      <h1 className="site-title brand">Cyber Career Paths</h1>
       <div className="nav-links">
         <Link to="/">Home</Link>
 
-        {/* Dropdown for Articles */}
-        <div className="dropdown">
-          <button className="dropbtn" onClick={toggleDropdown}>
+        {/* Articles dropdown (click to toggle) */}
+        <div className="dropdown" ref={dropdownRef}>
+          <button className="dropbtn" onClick={toggleDropdown} type="button">
             Articles ▾
           </button>
           {isOpen && (
