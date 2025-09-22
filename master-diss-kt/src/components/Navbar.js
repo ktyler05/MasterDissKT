eeffbc7f16df14017e848b3acc17f35920d19b5f
ktyler05 <tylerk@cardiff.ac.uk>
2025-09-22 23:36:15 +0100

    navbar update
    changed navbar added 2 more pages

--- master-diss-kt/src/components/Navbar.js
+++ master-diss-kt/src/components/Navbar.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles.css";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // close dropdown when a link is clicked
+  const closeDropdown = () => setIsOpen(false);
+
   return (
     <nav className="navbar">
       <h1 className="site-title">Cyber Career Paths</h1>
       <div className="nav-links">
         <Link to="/">Home</Link>
-        <Link to="/students">Students</Link>
-        <Link to="/parents">Parents</Link>
-        <Link to="/educators">Educators</Link>
-        <Link to="/policy">Policy</Link>
+
+        {/* Dropdown for Articles */}
+        <div className="dropdown">
+          <button className="dropbtn" onClick={toggleDropdown}>
+            Articles ▾
+          </button>
+          {isOpen && (
+            <div className="dropdown-content">
+              <Link to="/students" onClick={closeDropdown}>Students</Link>
+              <Link to="/parents" onClick={closeDropdown}>Parents</Link>
+              <Link to="/educators" onClick={closeDropdown}>Educators</Link>
+              <Link to="/policy" onClick={closeDropdown}>Policy</Link>
+            </div>
+          )}
+        </div>
+
+        <Link to="/about">About</Link>
+        <Link to="/thanks">Thanks</Link>
       </div>
     </nav>
   );
---
+++ master-diss-kt/src/pages/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+function About() {
+  return (
+    <div className="page">
+      <h2>
+        About page
+      </h2>
+
+     
+
+
+      <div className="divider" />
+
+
+    </div>
+  );
+}
+
+export default About;
