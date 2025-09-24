7bba375bfff88bbcd0be86720f9fbf730c30941e
ktyler05 <tylerk@cardiff.ac.uk>
2025-09-24 15:51:16 +0100

    fix again

--- master-diss-kt/src/App.js
+++ master-diss-kt/src/App.js
@@ -1,37 +1,30 @@
-// src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Students from "./pages/Students";
 import Parents from "./pages/Parents";
 import Educators from "./pages/Educators";
 import Policy from "./pages/Policy";
-import Thanks from "./pages/Thanks";
 import About from "./pages/About";
+import Thanks from "./pages/Thanks";
+import Footer from "./components/Footer";
 
-
-function App() {
+export default function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/students" element={<Students />} />
-            <Route path="/parents" element={<Parents />} />
-            <Route path="/educators" element={<Educators />} />
-            <Route path="/policy" element={<Policy />} />
-            <Route path="/thanks" element={<Thanks />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="app-container">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/students" element={<Students />} />
+        <Route path="/parents" element={<Parents />} />
+        <Route path="/educators" element={<Educators />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/thanks" element={<Thanks />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
-
-export default App;
